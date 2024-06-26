98dd9ae455081bf2b9471ab1a95bc24e5f140016
Sami Kudsi <skudsi490@gmail.com>
2024-06-26 17:02:56 +0200

    Update configuration v44

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
+echo "Starting entrypoint script..."
+
 # Wait for PostgreSQL to be ready
 while ! nc -z $POSTGRES_HOST 5432; do
   echo "Waiting for PostgreSQL..."
   sleep 1
 done
 
+echo "Applying database migrations..."
 # Apply database migrations
 python manage.py migrate
 
+echo "Checking for data_dump.json..."
 # Load data into PostgreSQL
 if [ -f /tmp/data_dump.json ]; then
   echo "Loading data into PostgreSQL"
   python manage.py loaddata /tmp/data_dump.json
+else
+  echo "data_dump.json not found."
 fi
 
+echo "Collecting static files..."
 # Collect static files
 python manage.py collectstatic --noinput
 
+echo "Creating log directory for Gunicorn if it doesn't exist..."
 # Create log directory for Gunicorn if it doesn't exist
 mkdir -p /var/log/gunicorn
 chmod -R 755 /var/log/gunicorn
