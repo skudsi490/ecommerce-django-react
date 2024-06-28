c53525f38f5d1e4f7e1f25e92301de08e47d9468
Sami Kudsi <skudsi490@gmail.com>
2024-06-28 12:04:52 +0200

    Update configuration v114

--- backend/tests/test_models.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-@pytest.mark.django_db
-def test_placeholder():
-    assert True
--- tests/api/test_products.py
+++ tests/api/test_products.py
@@ -1,7 +1,39 @@
-# tests/api/test_products.py
 import pytest
 
+# @pytest.mark.django_db
+# def test_product_created():
+#   Product.objects.create
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+
+from base.models import Product
+
+
+def create_product():
+  return Product.objects.create(
+        name=" Product Name ",
+        price=0,
+        brand="Sample brand ",
+        countInStock=0,
+        category="Sample category",
+        description=" ")
+
 @pytest.mark.django_db
+def test_product_creation():
+  p = create_product()
+  assert isinstance(p, Product) is True
+  assert p.name == " Product Name "
+
+
+
+
+
+# Api test  - Integration testing
 def test_api_product_creation():
-    # Simplified test to always pass
-    assert True
+    client = APIClient()
+
+    response = client.post("/api/products/create/")
+
+    # data = response.data
+
+    assert response.status_code == 200
